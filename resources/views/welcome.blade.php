1456c014ff85c0a929cc7f851c5d96f990914152
glena072 <glena072@gmail.com>
2022-07-26 16:01:27 +1000

    fixed can not find app.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
      <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 
      <!-- Styles -->
-     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+     {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
      <link rel="stylesheet" href="{{ mix('css/ui.css') }}">
      <link rel="stylesheet" href="{{ mix('css/responsive.css') }}">
      {{-- <link rel="stylesheet" href="{{ mix('css/style.css') }}"> --}}
