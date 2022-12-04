5a2248a6b589ebe1b484d6ab0acba056204f0f8a
Glen <glen@nudecode.com.au>
2022-12-04 12:55:33 +1000

    WIP fixing transactions

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="h4 font-weight-bold">
-            {{ __('Dashboard') }}
+            {{ __('Simple Budget') }}
         </h2>
     </x-slot>
 
