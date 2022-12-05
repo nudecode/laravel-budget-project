3bc84419224622dd64466efb08978b9994513f4c
glena072 <glena072@gmail.com>
2022-12-05 15:40:41 +1000

    updated welcome page and dashboard view

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -2,10 +2,10 @@
 <section class="content-main">
 	<div class="d-flex justify-content-between mb-3">
 		<div>
-			<a href="{{ route('transactions') }}" class="btn btn-outline-success">Income</a>
+			<a href="{{ route('transactions') }}" class="btn btn-outline-success">Income <span>{{ $incomes }}</span></a>
 		</div>
 		<div>
-			<a href="{{ route('transactions') }}" class="btn btn-outline-success">Expenses</a>
+			<a href="{{ route('transactions') }}" class="btn btn-outline-success">Expenses <span>{{ $expenses }}</span></a>
 		</div>
 		{{-- <div>
 			<a href="#" class="btn btn-primary">Income</a>
@@ -25,17 +25,17 @@
             <h5 class="card-title">Summary</h5>
 
 
-            <span class="text-muted">Balance</span><span class="d-block">{{$incomes->add($expenses)}}</span>
+            <span class="text-muted">Balance</span><span class="d-block">{{ $incomes->add($expenses) }}</span>
             <div class="progress mb-3">
             <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 15%">15%</div>
             </div>
 
-            <span class="text-muted">Income</span><span class="d-block">{{$incomes}}</span>
+            <span class="text-muted">Income</span><span class="d-block">{{ $incomes }}</span>
             <div class="progress mb-3">
             <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 65%">65% </div>
             </div>
 
-            <span class="text-muted">Expense</span><span class="d-block">{{$expenses}}</span>
+            <span class="text-muted">Expense</span><span class="d-block">{{ $expenses }}</span>
             <div class="progress mb-3">
             <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 51%"> 51% </div>
             </div>
